0bbadce16b93b59c9564fce51e9f529db45ce50d
Russell Schmidt <russell.schmidt@me.com>
2018-02-21 12:31:56 -0800

    add hero image

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,14 @@
 import React from "react";
 import Helmet from "react-helmet";
 import Link from 'gatsby-link';
+import { css } from "glamor";
+
+const heroImage = css({
+  width: `100vw`
+});
 
 export default () => (
   <div>
-    <p>Thanks for stopping by!</p>
+    <img className={heroImage} src="https://s3.amazonaws.com/russell-personal/rs/anothercityispossible_comp1.jpg" title="truth" alt="another city is possible"/>
   </div>
 );
